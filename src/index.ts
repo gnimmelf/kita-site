eeb1efbe01f667b52b0b574d464daa333447490f
Flemming Hansen <gnimmelf@gmail.com>
2024-07-28 16:32:56 +0200

    Port settable by env.PORT

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { createApp } from './app'
 
+
 const bootstrap = async () => {
-    const app = createApp({ port: 3000 })
+    const app = createApp({ port: process.env.PORT || 3000 })
 }
 
 bootstrap();
