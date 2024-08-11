d223693a358e57b7d4abd6ea107c7c9a80fdb4de
Flemming Hansen <gnimmelf@gmail.com>
2024-08-11 12:12:08 +0200

    Changed, moved fonts

--- src/theme/Layout.tsx
+++ src/theme/Layout.tsx
@@ -51,9 +51,6 @@ const Layout: Component<{
                 {'<!doctype html>'}
                 <html lang="en">
                     <head>
-                        <style>
-                            @import url('https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap');
-                        </style>                        
                         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                         <meta http-equiv="Pragma" content="no-cache" />
                         <meta http-equiv="Expires" content="0" />
