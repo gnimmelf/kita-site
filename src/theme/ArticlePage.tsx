50ff6e3e10221fd31af2a8dbcdc1eba7465c449a
Flemming Hansen <gnimmelf@gmail.com>
2024-08-12 17:31:41 +0200

    ArticlePage font-styling

--- src/theme/ArticlePage.tsx
+++ src/theme/ArticlePage.tsx
@@ -10,20 +10,30 @@ import { createSheet } from './styles'
 
 const { classes } = createSheet({
     article: {
-        padding: '10px',
+        padding: '10px 15px',
         maxWidth: 'var(--content-width)',
         margin: '0 auto',
         backgroundColor: 'var(--card-bg)',
         color: 'var(--card-fg)',
         border: '2px solid',
         borderColor: 'var(--card-border)',
-        borderRadius: 'var(--border-radius)',    
+        borderRadius: 'var(--border-radius)',
         '& a': {
             color: 'var(--card-accent)',
-        },
+        }
     },
     title: {
-        textTransform: 'capitalize'
+        textTransform: 'capitalize',
+        fontSize: '1.7rem'
+    },
+    body: {
+        '& > p': {
+            fontSize: '1rem'            
+        },
+        '& > pre': {
+            padding: '5px',
+            overflowX: 'scroll',
+        }
     },
     backLink: {
         textAlign: 'center'
@@ -41,8 +51,9 @@ export const ArticlePage: Component<{
             <Layout ctx={ctx} pageTitle={article.meta.title}>
                 <section class={classes.article}>
                     <h1 class={classes.title}>{article.meta.title}</h1>
-                    <div>{article.body}</div>
+                    <div class={classes.body}>{article.body}</div>
                     <div class={classes.backLink}>
+                        <div>~ ~ ~</div>
                         <a href="/">Back</a>
                     </div>
                 </section>
